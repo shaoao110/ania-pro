75043bf2cf716d4b6147d187334db477d7918163
shaoao110 <670207420@qq.com>
2023-06-19 15:20:16 +0800

    feat: update and add new builds

--- build/builds.py
+++ build/builds.py
@@ -24,6 +24,21 @@ class BuildList():
     def __init__(self, bot):
         if bot != None:
             # 配装指令
+            @bot.command(regex=r'/hdzp|火电重炮',case_sensitive=False)
+            async def huodianzhongpao(msg: Message):
+                await reqFrontLogger(msg,bot)
+                await msg.reply(Build().getBuild('huodianzhongpao'))
+
+            @bot.command(regex=r'/hd|火电',case_sensitive=False)
+            async def huodian(msg: Message):
+                await reqFrontLogger(msg,bot)
+                await msg.reply(Build().getBuild('huodian'))
+
+            @bot.command(regex=r'/jgj|坚果狙',case_sensitive=False)
+            async def jianguoju(msg: Message):
+                await reqFrontLogger(msg,bot)
+                await msg.reply(Build().getBuild('lieyanhabao'))
+
             @bot.command(regex=r'/zp|重炮|极限重炮',case_sensitive=False)
             async def zhongpao(msg: Message):
                 await reqFrontLogger(msg,bot)
@@ -137,37 +152,44 @@ class BuildList():
             logger.info("Build Commands Load Success")
 
     def getList(self) -> CardMessage:
-        c = Card(Module.Header('常用配装指南（配装表2.0）'), color='#FFCC00')
+        c = Card(Module.Header('常用配装指南（配装表2.1）'), color='#FFCC00')
         c.append(Module.Section({"type":"kmarkdown","content":"萌新配装推荐路线图请参考指令：`/map`"}))
         c.append(Module.Context('在文字频道发送配装对应指令，Ania会回复你具体配装信息，多个快捷指令任选其一即可'))
         # 全火输出
         c.append(Module.Divider())
-        c.append(Module.Section('全火输出(6)'))
+        c.append(Module.Section('全火输出(9)'))
         c.append(Module.Context('全部属性致力于提升武器伤害，不关注生存，追求极致的输出能力，但身板很脆，在有队友辅助的情况下是绝对的团队输出核心'))
         c.append(Module.Divider())
-        c.append(Module.Section({"type":"kmarkdown","content":"全火探子警戒(推荐)     快捷指令：`/tj` `探子警戒`"}))
-        c.append(Module.Section({"type":"kmarkdown","content":"全火突袭(推荐)            快捷指令：`/tx` `突袭`"}))
-        c.append(Module.Section({"type":"kmarkdown","content":"谈判专注AR(高群伤)    快捷指令：`/tp` `谈判`"}))
-        c.append(Module.Section({"type":"kmarkdown","content":"金装全火AR(三天命)    快捷指令：`/qh` `全火` `金装全火`"}))
-        c.append(Module.Section({"type":"kmarkdown","content":"金装全火AR(无天命)    快捷指令：`/qh2` `全火2` `金装全火2`"}))
-        c.append(Module.Section({"type":"kmarkdown","content":"打牌猎头狙                 快捷指令：`/dp` `打牌` `打牌狙`"}))
-        c.append(Module.Section({"type":"kmarkdown","content":"先锋猎头狙                 快捷指令：`/xf` `先锋` `先锋狙`"}))
-        c.append(Module.Section({"type":"kmarkdown","content":"王子手枪盾                 快捷指令：`/wz` `王子`"}))
+        c.append(Module.Section({"type":"kmarkdown","content":"雷神突袭(T0火爆)              快捷指令：`/tx` `突袭`"}))
+        c.append(Module.Section({"type":"kmarkdown","content":"烈焰哈堡坚果狙(T0暴力无脑)  快捷指令：`/jgj` `坚果狙`"}))
+        c.append(Module.Section({"type":"kmarkdown","content":"王子手枪盾(T1)                 快捷指令：`/wz` `王子`"}))
+        c.append(Module.Section({"type":"kmarkdown","content":"全火探子警戒(T1推荐)       快捷指令：`/tj` `探子警戒`"}))
+        c.append(Module.Section({"type":"kmarkdown","content":"谈判专注AR(T1高群伤)      快捷指令：`/tp` `谈判`"}))
+        c.append(Module.Section({"type":"kmarkdown","content":"打牌猎头狙(T1)                 快捷指令：`/dp` `打牌` `打牌狙`"}))
+        c.append(Module.Section({"type":"kmarkdown","content":"先锋猎头狙(T1)                 快捷指令：`/xf` `先锋` `先锋狙`"}))
+        c.append(Module.Section({"type":"kmarkdown","content":"金装全火AR(T2三天命)      快捷指令：`/qh` `全火` `金装全火`"}))
+        c.append(Module.Section({"type":"kmarkdown","content":"金装全火AR(T2无天命)      快捷指令：`/qh2` `全火2` `金装全火2`"}))
         # 火体生存
         c.append(Module.Divider())
         c.append(Module.Section('火体生存(4)'))
         c.append(Module.Context('兼顾进攻与防守，往往依靠高阶盾牌和叠加大量的额外装甲抵御敌方的攻击，同时能够打出可观的伤害，是吸引仇恨、保护队友、冲锋陷阵的首选'))
         c.append(Module.Divider())
-        c.append(Module.Section({"type":"kmarkdown","content":"猎人追悼天蝎座          快捷指令：`/lr` `猎人` `猎人喷`"}))
-        c.append(Module.Section({"type":"kmarkdown","content":"火体芳心                    快捷指令：`/fx` `芳心` `火体芳心`"}))
+        c.append(Module.Section({"type":"kmarkdown","content":"猎人追悼天蝎座(T1)    快捷指令：`/lr` `猎人` `猎人喷`"}))
+        c.append(Module.Section({"type":"kmarkdown","content":"火体芳心(T1)              快捷指令：`/fx` `芳心` `火体芳心`"}))
         c.append(Module.Section({"type":"kmarkdown","content":"精防喷子                    快捷指令：`/jf` `精防` `精防喷子`"}))
         c.append(Module.Section({"type":"kmarkdown","content":"精防芳心                    快捷指令：`/jffx` `精防芳心`"}))
+        # 火电
+        c.append(Module.Divider())
+        c.append(Module.Section('火电(4)'))
+        c.append(Module.Context('拥有高技能分阶的同时，凭借电容突击的特点也拥有高武器伤害，即可使用武器输出，同时也可以根据战场需要灵活搭配技能做到多点输出或全能辅助'))
+        c.append(Module.Section({"type":"kmarkdown","content":"忍者包火电突袭(7火6电)  快捷指令：`/hd` `火电`"}))
+        c.append(Module.Section({"type":"kmarkdown","content":"忍者包火电重炮(6火6电)  快捷指令：`/hdzp` `火电重炮`"}))
         # 电工塔防
         c.append(Module.Divider())
         c.append(Module.Section('电工塔防(3)'))
         c.append(Module.Context('以技能为主要输出手段，输出手法比较简单，适用于难以露头输出的极端场景或依靠技能的强大火力对敌人形成压制，甚至堵门输出，技能的多样性决定了电工各种场景的高适应性'))
         c.append(Module.Divider())
-        c.append(Module.Section({"type":"kmarkdown","content":"波皇塔防                    快捷指令：`/tf` `塔防`"}))
+        c.append(Module.Section({"type":"kmarkdown","content":"波皇塔防(必备)           快捷指令：`/tf` `塔防`"}))
         c.append(Module.Section({"type":"kmarkdown","content":"极限重炮                    快捷指令：`/zp` `重炮` `极限重炮`"}))
         c.append(Module.Section({"type":"kmarkdown","content":"极限氧化剂                 快捷指令：`/yhj` `氧化剂`"}))
         # 辅助增伤
@@ -175,8 +197,8 @@ class BuildList():
         c.append(Module.Section('辅助增伤(3)'))
         c.append(Module.Context('为团队队友提供增伤、控场、治疗等辅助手段，是团队不可或缺的一员，一名经验丰富的辅助可以使整个团队战力倍增'))
         c.append(Module.Divider())
-        c.append(Module.Section({"type":"kmarkdown","content":"超载未来奶妈              快捷指令：`/nm` `奶妈` `未来奶`"}))
-        c.append(Module.Section({"type":"kmarkdown","content":"日蚀火法控制              快捷指令：`/rs` `日蚀` `日蚀控`"}))
+        c.append(Module.Section({"type":"kmarkdown","content":"超载未来奶妈(必备)     快捷指令：`/nm` `奶妈` `未来奶`"}))
+        c.append(Module.Section({"type":"kmarkdown","content":"日蚀火法控制(必备)     快捷指令：`/rs` `日蚀` `日蚀控`"}))
         c.append(Module.Section({"type":"kmarkdown","content":"政令极限增伤(推荐)     快捷指令：`/zl` `政令`"}))
         # Raid常用
         c.append(Module.Divider())
--- image_comm.py
+++ image_comm.py
@@ -34,42 +34,42 @@ class ImageServe():
             @bot.command(name='强化')
             async def material(msg: Message):
                 await reqFrontLogger(msg,bot)
-                url = await bot.client.create_asset('./img/material.png')
-                await msg.reply(url, type=MessageTypes.IMG)
+                # url = await bot.client.create_asset('./img/material.png')
+                await msg.reply('https://img.kaiheila.cn/assets/2022-06/20/g5r29OKbDt0us214.png', type=MessageTypes.IMG)
 
             @bot.command(name='材料')
             async def cailiao(msg: Message):
                 await reqFrontLogger(msg,bot)
-                url = await bot.client.create_asset('./img/cailiao.jpg')
-                await msg.reply(url, type=MessageTypes.IMG)
+                # url = await bot.client.create_asset('./img/cailiao.jpg')
+                await msg.reply('https://img.kaiheila.cn/assets/2022-07/19/Pu58Fd6hUW1tm3w2.jpg', type=MessageTypes.IMG)
 
             # 显示配装列表
             @bot.command(regex=r'/map|路线图')
             async def buildmap(msg: Message):
                 await reqFrontLogger(msg,bot)
-                url = await bot.client.create_asset('./img/map.png')
-                await msg.reply(url, type=MessageTypes.IMG)
+                # url = await bot.client.create_asset('./img/map.png')
+                await msg.reply('https://img.kaiheila.cn/attachments/2022-09/29/TdSoBslIZT0zq1j5.png', type=MessageTypes.IMG)
 
             # 技能增伤
             @bot.command(name='技能')
             async def jineng(msg: Message):
                 await reqFrontLogger(msg,bot)
-                url = await bot.client.create_asset('./img/jineng.png')
-                await msg.reply(url, type=MessageTypes.IMG)
+                # url = await bot.client.create_asset('./img/jineng.png')
+                await msg.reply('https://img.kaiheila.cn/assets/2022-06/16/GLgWT5AnNm0ck0cl.png', type=MessageTypes.IMG)
 
             # 突击步枪性能排行
             @bot.command(name='ar')
             async def arrank(msg: Message):
                 await reqFrontLogger(msg,bot)
-                url = await bot.client.create_asset('./img/arrank.png')
-                await msg.reply(url, type=MessageTypes.IMG)
+                # url = await bot.client.create_asset('./img/arrank.png')
+                await msg.reply('https://img.kaiheila.cn/attachments/2022-10/09/Cbhkm4Z5jz0y80sd.png', type=MessageTypes.IMG)
 
             # 赛季日历
             @bot.command(regex=r'/rl|日历|赛季日历')
             async def calane(msg: Message):
                 await reqFrontLogger(msg,bot)
-                url = await bot.client.create_asset('./img/calander_season11.png')
-                await msg.reply(url, type=MessageTypes.IMG)
+                # url = await bot.client.create_asset('./img/calander_season12.jpg')
+                await msg.reply('https://img.kookapp.cn/attachments/2023-06/19/6DToxK9pwG1q20yw.jpg', type=MessageTypes.IMG)
 
             # 赛季日历
             @bot.command(regex=r'/jy|活动经验')
@@ -82,27 +82,27 @@ class ImageServe():
             @bot.command(regex=r'/aq|暗区')
             async def anqu(msg: Message):
                 await reqFrontLogger(msg,bot)
-                url1 = await bot.client.create_asset('./img/xianqu.png')
-                url2 = await bot.client.create_asset('./img/nananqu.png')
-                url3 = await bot.client.create_asset('./img/donganqu.png')
-                await msg.reply(url1, type=MessageTypes.IMG)
-                await msg.reply(url2, type=MessageTypes.IMG)
-                await msg.reply(url3, type=MessageTypes.IMG)
+                # url1 = await bot.client.create_asset('./img/xianqu.png')
+                # url2 = await bot.client.create_asset('./img/nananqu.png')
+                # url3 = await bot.client.create_asset('./img/donganqu.png')
+                await msg.reply('https://img.kookapp.cn/attachments/2022-10/26/Z2nADRlc0s1j70v2.png', type=MessageTypes.IMG)
+                await msg.reply('https://img.kookapp.cn/attachments/2022-10/26/y4QSQoo3UA1j70v1.png', type=MessageTypes.IMG)
+                await msg.reply('https://img.kookapp.cn/attachments/2022-10/28/f9VQo0xxiQ1j90ug.png', type=MessageTypes.IMG)
 
             # 套装
             @bot.command(regex=r'/tz')
             async def taozhuang(msg: Message):
                 await reqFrontLogger(msg,bot)
-                url1 = await bot.client.create_asset('./img/taozhuang1.jpg')
-                url2 = await bot.client.create_asset('./img/taozhuang2.jpg')
-                url3 = await bot.client.create_asset('./img/taozhuang3.jpg')
-                await msg.reply(url1, type=MessageTypes.IMG)
-                await msg.reply(url2, type=MessageTypes.IMG)
-                await msg.reply(url3, type=MessageTypes.IMG)
+                # url1 = await bot.client.create_asset('./img/taozhuang1.jpg')
+                # url2 = await bot.client.create_asset('./img/taozhuang2.jpg')
+                # url3 = await bot.client.create_asset('./img/taozhuang3.jpg')
+                await msg.reply('https://img.kookapp.cn/attachments/2022-11/08/td7tsBs7UW1ib1ku.jpg', type=MessageTypes.IMG)
+                await msg.reply('https://img.kookapp.cn/attachments/2022-11/08/762XAy7PXr1ib23m.jpg', type=MessageTypes.IMG)
+                await msg.reply('https://img.kookapp.cn/attachments/2022-11/08/cOIKn0X0wa1ib2d9.jpg', type=MessageTypes.IMG)
 
             # 装备天赋强度
             @bot.command(name='装备天赋')
             async def abtalent(msg: Message):
                 await reqFrontLogger(msg,bot)
-                url = await bot.client.create_asset('./img/abtalent.png')
-                await msg.reply(url, type=MessageTypes.IMG)
+                # url = await bot.client.create_asset('./img/abtalent.png')
+                await msg.reply('https://img.kaiheila.cn/attachments/2022-10/09/wKAV1xp5k20l10g8.png', type=MessageTypes.IMG)
